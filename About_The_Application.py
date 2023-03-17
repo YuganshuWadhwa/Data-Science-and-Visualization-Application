ebe46ecb68fde165beaef61661d825f9dbf037ae
nomad <ywadhwa99@gmail.com>
2023-03-17 02:53:01 +0100

    Added to navigation on main page

--- About_The_Application.py
+++ About_The_Application.py
@@ -134,14 +134,15 @@ with features_cont :
 
 		st.markdown("""
 
-		- The application is structured as a ***collection of multiple pages*** performing certain individual functions
-		- These pages can be ***accessed from the side-bar***
-		- On the page ***"Data Upload and Preview"***, the working dataset can be chosen or uploaded
-		- It is necessary to ***load the "Data Upload and Preview"*** page and ***select / upload / change the working dataset***, before proceeding any further through the application
-		- In order to generate the ***Processed Data*** for regression, please use the ***Outlier Recognition***, ***Interpolation*** and ***Smoothing*** (in the mentioned order)
-		- On each page, some ***generic*** and ***method- specific*** information is provided
-		- Look out for ***hints*** :bulb: on each page for further assistance regarding the ***type of datasets the page works best with***
-		- In some of the pages, ***tooltips*** :grey_question: are provided for further assistance regarding the ***parameters***
+		- The application is structured as a ***collection of multiple pages*** performing certain individual functions.
+		- These pages can be ***accessed from the side-bar***.
+		- On the page ***"Data Upload and Preview"***, the working dataset can be chosen or uploaded.
+		- It is necessary to ***load the "Data Upload and Preview"*** page and ***select / upload / change the working dataset***, before proceeding any further through the application.
+		- In order to generate the ***Processed Data*** for regression, please use the ***Outlier Recognition***, ***Interpolation*** and ***Smoothing*** (in the mentioned order).
+		- After choosing the desired parameters on the "Data Smoothing" page, ***please press "Smoothen Complete Dataframe"*** to finally generate processed data.
+		- On each page, some ***generic*** and ***method- specific*** information is provided.
+		- Look out for ***hints*** :bulb: on each page for further assistance regarding the ***type of datasets the page works best with***.
+		- In some of the pages, ***tooltips*** :grey_question: are provided for further assistance regarding the ***parameters***.
 
 		""")
 
